e37b26c9c3c7437c8ed8dc7e2eb0f61eef434f12
shahmojno <mojno12@gmail.com>
2022-04-09 19:18:05 +0600

    Adding about us

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
         <div>
-            <h3>About Us</h3>
-        </div>
+            <h3 className='about-us'>About Us</h3>
+
+            <footer className='footer-container'>
+                <h3>
+                    Copyright@
+                </h3>
+                <h4>
+                    © 2022 All rights reserved
+                </h4>
+            </footer>
+        </div >
     );
 };
 
